5892aef89c7208f5bf9d48b4d1f1ac230d2fe6a5
kawaljeetsingh <kawaljeet.singh@osscube.com>
2013-05-06 03:05:38 -0700

    validate class

--- libraries/validate.php
+++ libraries/validate.php
@@ -1,12 +1,30 @@
 <?php
+/*
+ * FileName: Validate.php
+* Version: 1.0
+* Author: Keshi Chander Yadav, Tanu Trehan, Manish
+* Date: May 03, 2013
+* Description: Validate class
+*/
+
 require_once('validation.class.php');
 class validate
 {
-    private  $obj;
+    private  $obj;	//Class Object Reference
     private $errorMsg="";
+    
     function __construct(){
         $this->obj = new validation();
     }
+    
+    /*
+	 * Description: function to validate fields
+	 * to this function
+	 * Param $controler_name field control name goes here
+	 * Param $postVar field data to be validated
+	 * Param $authType Type of validation required for control
+	 * Param $error error message for in case of validation failed
+	 */
     function validator($name,$postVar=" ", $authType, $error)
     {
         $auth=explode('#', $authType);
@@ -27,6 +45,8 @@ class validate
         }
 
     }
+    
+    //return validation result with error messages
     function result()
     {
         $this->errorMsg=$this->obj->validate();
