8201b406b92a6b20960c4659300d0a89d15f7b11
ramandeepsinghoss <ramandeep.singh@osscube.com>
2013-05-09 22:18:39 -0700

    encryption class added

---
+++ libraries/EncryptionDecryption.php
@@ -0,0 +1,53 @@
+<?php
+class EncryptionDecryption
+{
+    private $skey;
+    
+    private function createKey() {
+    
+        $password = 1;
+        $salt = "rs2s0e1k3etieealmkle".mktime();
+        $hash = hash_hmac("md5", $password, $salt);
+        $this->skey = pack('H*', $hash);
+    }
+    
+    private function safe_b64encode($string) {
+    
+        $data = base64_encode($string);
+        $data = str_replace(array('+','/','='),array('-','_',''),$data);
+        return $data;
+    }
+    
+    private function safe_b64decode($string) {
+        $data = str_replace(array('-','_'),array('+','/'),$string);
+        $mod4 = strlen($data) % 4;
+        if ($mod4) {
+            $data .= substr('====', $mod4);
+        }
+        return base64_decode($data);
+    }
+    
+    public  function encode($value){
+    
+        if(!$value){return false;}
+    
+        $this->createKey();
+        $text = $value;
+        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+        $crypttext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->skey, $text, MCRYPT_MODE_ECB, $iv);
+        return trim($this->safe_b64encode($crypttext));
+    }
+    
+    public function decode($value){
+    
+        if(!$value){return false;}
+        $crypttext = $this->safe_b64decode($value);
+        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+        $decrypttext = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->skey, $crypttext, MCRYPT_MODE_ECB, $iv);
+        return trim($decrypttext);
+    }
+}
+
+?>
